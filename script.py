1e9ed6860df93a002da047e6da4b7f2f84021f66
unknown <pulkitpahuja98@gmail.com>
2019-03-30 15:13:06 +0530

    Added Blinking

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import dlib
 import numpy as np
 import cv2
+from math import hypot
 
 irises=[]
 
@@ -24,6 +25,24 @@ def get_irises_location(frame_gray):
 
     return irises
 
+def get_blinking_ratio(eye_points, facial_landmarks):
+    left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+    right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+
+    #hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+    #ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+
+    hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+    ver_line_lenght = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+
+    ratio = hor_line_lenght / ver_line_lenght
+    return ratio
+
+def midpoint(p1 ,p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
 
 while True:
     d=[]
@@ -39,6 +58,13 @@ while True:
         #cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
 
         landmarks = predictor(gray, face)
+
+        left_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
+        right_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
+        blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
+
+        if blinking_ratio > 5.7:
+            cv2.putText(frame, "BLINKING", (50, 150), font, 7, (255, 0, 0))
 
         left_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
                                     (landmarks.part(37).x, landmarks.part(37).y),
